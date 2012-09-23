ccfe0044ce28c23b7a5a31571b5a94931b9aace9
Johan Lyheden <johan@lyheden.com>
2012-09-24 00:40:21 +0200

    Added more methods in the base provider

--- lib/puppet/provider/zabbixprovider.rb
+++ lib/puppet/provider/zabbixprovider.rb
@@ -14,20 +14,46 @@ class Puppet::Provider::Zabbixprovider < Puppet::Provider
   end
 
   def zbxapi_get_host_from_name(name,output,token)
-    host = token.host.get( { 'output' => output, 'filter' => { 'host' => [ name ] } } )
+    host = token.host.get( { 'output' => output, 'filter' => { 'host' => name } } )
     return host if host.length > 0
     return []
   end
 
   def zbxapi_get_host_groups_from_name(name,output,token)
-    host = token.host.get( { 'select_groups' => output, 'filter' => { 'host' => [ name ] } } )
+    host = token.host.get( { 'select_groups' => output, 'filter' => { 'host' => name } } )
     return host[0]['groups'] if host.length > 0 and host[0].has_key? 'groups'
     return []
   end
 
+  def zbxapi_get_host_templates_from_name(name,output,token)
+    host = token.host.get( { 'selectParentTemplates' => output, 'filter' => { 'host' => name } } )
+    return host[0]['parentTemplates'] if host.length > 0 and host[0].has_key? 'parentTemplates'
+    return []
+  end
+
+  def zbxapi_get_template_from_name(name, output, token)
+    # The host.get_template API method is working against the API docs, not returning an array but instead a hash directly
+    # which each templateid as key containing the resulting data
+    # This method generates a list with the data instead to be consistent with rest of API
+    # NOT TRUE, seem to be fixed in 1.8.6, need to add api version checker
+    template = token.host.get_template( { 'output' => output, 'filter' => { 'host' => name } } )
+    return template[0] if template.length > 0
+    return []
+    #return_template = []
+    #template.each_pair { |k,v| return_template << v }
+    #return return_template[0] if return_template.length > 0
+    #return []
+  end
+
   def zbxapi_get_group_from_name(name,output,token)
-    group = token.hostgroup.get( { 'output' => output, 'filter' => { 'name' => [ name ] } } )
+    group = token.hostgroup.get( { 'output' => output, 'filter' => { 'name' => name } } )
     return group[0] if group.length > 0
+    return []
+  end
+
+  def zbxapi_get_host_macros_from_name(name,output,token)
+    host = token.host.get( { 'select_macros' => output, 'filter' => { 'host' => name } } )
+    return host[0]['macros'] if host.length > 0 and host[0].has_key? 'macros'
     return []
   end
 
